48239be1e8d2b0c425d1297a897fa88f9a5bf227
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-31 14:49:23 +0200

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by ramil on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
